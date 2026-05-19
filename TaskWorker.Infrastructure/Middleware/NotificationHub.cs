dc2bda7a3cecdc84884b87d5f073203df222ebd6
Shoriful Islam Robi <robi@itcbd.com>
2026-05-19 16:11:01 +0600

    SignalR using

--- TaskWorker.Infrastructure/Middleware/NotificationHub.cs
+++ TaskWorker.Infrastructure/Middleware/NotificationHub.cs
@@ -10,22 +10,22 @@ namespace TaskWorker.Infrastructure.Middleware
             await Clients.All.SendAsync("ReceiveNotification", title, message);
         }
 
-        public async Task NotifyIssueAssignment(string message)
+        public async Task NotifyIssueAssignment(string title, string message)
         {
             await Clients.All.SendAsync("ReceiveIssueAssignment", message);
         }
 
-        public async Task NotifyReview(string message)
+        public async Task NotifyReview(string title, string message)
         {
             await Clients.All.SendAsync("ReceiveReview", message);
         }
 
-        public async Task NotifyTransfer(string message)
+        public async Task NotifyTransfer(string title, string message)
         {
             await Clients.All.SendAsync("ReceiveTransfer", message);
         }
 
-        public async Task NotifyClose(string message)
+        public async Task NotifyClose(string title, string message)
         {
             await Clients.All.SendAsync("ReceiveClose", message);
         }
--- TaskWorker.Infrastructure/Utility/JwtConfig.cs
+++ TaskWorker.Infrastructure/Utility/JwtConfig.cs
@@ -32,6 +32,7 @@ namespace TaskWorker.Infrastructure.Utility
 
                 auth.TokenExpired = DateTime.UtcNow.AddMinutes(expirationTime);
                 var claims = new[] {
+
                     new Claim(type: "JWTId", jti),
                     new Claim(ClaimTypes.NameIdentifier, auth.UserId.ToString()),
                     new Claim(type: "UserId", value: auth.UserId.ToString() ?? ""),
